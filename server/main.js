c6b1185f8fdb1ece22fe6158590ef9f9927904d7
dr.dimitru <ceo@veliov.com>
2021-04-21 22:27:09 +0300

    In-code docs
    + Expose `profiles` collection via `app`

--- server/main.js
+++ server/main.js
@@ -1,10 +1,22 @@
 import { Meteor } from 'meteor/meteor';
 
 import { app } from '/imports/lib/app.js';
+import { profiles } from '/imports/server/profiles/lib.js';
 
 import '/imports/server/service-configurations.js';
 import '/imports/server/methods';
 
+/*
+ * Get authenticated (via GitHub) user details, otherwise throw an error
+ * Used in Meteor.Methods to ensure user
+ *
+ * @namespace app
+ * @method checkUser
+ * @param {String} _id - User's _id
+ * @param {Object} [fields] - Optional Mongo Fields Specifier object. By default it would pick `profile` and `user.services.github` props
+ * @throws {Meteor.Error}
+ * @returns {Object} - User's object with required fields
+ */
 app.checkUser = (_id, fields) => {
   if (!_id) {
     throw new Meteor.Error(401, 'Please, login');
@@ -25,5 +37,7 @@ app.checkUser = (_id, fields) => {
 
   return user;
 };
+
+app.profiles = profiles;
 
 export { app };
