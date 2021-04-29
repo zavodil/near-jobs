dcf60070c8394d4208957be864d77f0b9f5b6ef6
dr.dimitru <ceo@veliov.com>
2021-04-29 11:04:34 +0300

    App library

--- imports/lib/app.js
+++ imports/lib/app.js
@@ -1,7 +1,11 @@
 import { Meteor } from 'meteor/meteor';
+import { collections } from '/imports/lib/collections';
 
 const app = {
-  
+  collections,
+  slugify(str) {
+    return str.toString().toLowerCase().trim().replace(/\s+/g, '-').replace(/[^\w\-]+/g, '').replace(/\-\-+/g, '-').replace(/^-+/, '').replace(/-+$/, '');
+  }
 };
 
 export { app };
