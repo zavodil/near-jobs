6d8866665277d3729204b5768ad823ecf1881893
dr.dimitru <ceo@veliov.com>
2021-04-29 14:47:14 +0300

    Describe routing helpers

--- imports/client/router/helpers.js
+++ imports/client/router/helpers.js
@@ -1,13 +1,33 @@
 import { app } from '/imports/lib/app.js';
 
+/**
+ * Handle global first navigation/load logic
+ * @type {boolean} firstTime
+ */
 let firstTime = true;
+
+/**
+ * Handle global root element for templates rendering
+ * @type {HTMLElement} loadingElement
+ */
 let loadingElement;
+
+/**
+ * Ensure and assign `loadingElement`
+ * @function ensureLoadingElement
+ * @returns {void 0}
+ */
 const ensureLoadingElement = () => {
   if (!loadingElement) {
     loadingElement = document.getElementById('loadingElement');
   }
 };
 
+/**
+ * Handle animation upon entry navigation logic
+ * @function onRendered
+ * @returns {void 0}
+ */
 const onRendered = function () {
   ensureLoadingElement();
   setTimeout(() => {
@@ -19,6 +39,11 @@ const onRendered = function () {
   }, 512);
 };
 
+/**
+ * Handle loading animation between routes
+ * @function whileWaiting
+ * @returns {void 0}
+ */
 const whileWaiting = function () {
   ensureLoadingElement();
   if (!firstTime) {
@@ -27,12 +52,21 @@ const whileWaiting = function () {
   loadingElement.style.display = 'flex';
 };
 
+/**
+ * Render templates upon navigation and handle animation timings logic
+ * @function render
+ * @returns {void 0}
+ */
 const render = (route, ...args) => {
   setTimeout(() => {
     route.render(...args, onRendered);
   }, 512);
 };
 
+/**
+ * Shortcut object holding widely-used templates names
+ * @type {object} templates
+ */
 const templates = {
   layout: 'layout'
 };
