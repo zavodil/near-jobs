7dfd6bad111dd8d46598a91e864269242e583984
dr.dimitru <ceo@veliov.com>
2021-05-02 01:58:10 +0300

    Update app.js

--- imports/lib/app.js
+++ imports/lib/app.js
@@ -1,7 +1,4 @@
-import collections from '/imports/lib/collections';
-
 const app = {
-  collections,
   slugify(str) {
     return str.toString().toLowerCase().trim().replace(/\s+/g, '-').replace(/[^\w\-]+/g, '').replace(/\-\-+/g, '-').replace(/^-+/, '').replace(/-+$/, '');
   }
