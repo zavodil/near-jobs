ea641abc6dc8fc6d3db1fbb140ebcafc8eaee468
dr.dimitru <ceo@veliov.com>
2021-05-02 01:55:46 +0300

    Profile button

--- imports/client/components/layout/layout.js
+++ imports/client/components/layout/layout.js
@@ -1,17 +1,12 @@
 import { app } from '/imports/lib/app.js';
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 
 import '/imports/client/components/login-form/login-form.js';
 import './animations.sass';
 import './layout.sass';
 import './layout.jade';
-
-Template.loginLogoutButton.onCreated(function () {
-  this.loggingOut = new ReactiveVar(false);
-});
 
 Template.layout.helpers({
   getYear() {
@@ -22,27 +17,5 @@ Template.layout.helpers({
   },
   canYeild() {
     return !!Meteor.userId() || FlowRouter._current.route.options?.isPublic === true;
-  }
-});
-
-Template.loginLogoutButton.helpers({
-  loggingOut() {
-    return Template.instance().loggingOut.get();
-  }
-});
-
-Template.loginLogoutButton.events({
-  'click [data-logout]'(e, template) {
-    e.preventDefault();
-    template.loggingOut.set(true);
-    setTimeout(() => {
-      Meteor.logout((error) => {
-        template.loggingOut.set(false);
-        if (error) {
-          console.error(error);
-        }
-      });
-    }, 1024);
-    return false;
   }
 });
