2a87c15938cda4eb119e72d6e5753c20485eefb3
dr.dimitru <ceo@veliov.com>
2021-05-08 01:11:44 +0300

    Publish job by its issues number

--- imports/server/collections/jobs.collection.js
+++ imports/server/collections/jobs.collection.js
@@ -1,3 +1,6 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { createIndex } from '/imports/server/helpers.js';
 import { jobs } from '/imports/lib/collections/jobs.collection.js';
 
 jobs.deny({
@@ -11,3 +14,14 @@ jobs.deny({
     return true;
   }
 });
+
+Meteor.publish('job', function (number) {
+  check(number, String);
+  if (!this.userId) {
+    return [];
+  }
+
+  return jobs.find({ number });
+});
+
+createIndex(jobs, { number: 1 }, { background: false });
