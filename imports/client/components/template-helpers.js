708c03cb06f3ef66c5a251f50e4671c44be95808
dr.dimitru <ceo@veliov.com>
2021-05-10 00:37:43 +0300

    Get Meteor.settings via templste

--- imports/client/components/template-helpers.js
+++ imports/client/components/template-helpers.js
@@ -1,4 +1,6 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { app } from '/imports/lib/app.js';
 
 Template.registerHelper('isSmall', () => app.isSmall);
+Template.registerHelper('meteorSettings', () => Meteor.settings.public);
