23cb21b0ab9dd3a82c4becdf4d6fe62c01a70e2e
dr.dimitru <ceo@veliov.com>
2021-05-11 17:33:14 +0300

    Job: Better mobile UI

--- imports/client/components/job-preview/job-preview.js
+++ imports/client/components/job-preview/job-preview.js
@@ -1,9 +1,18 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
 import './job-preview.jade';
 
 Template.jobPreview.helpers({
   getFirst(arr) {
-    return arr[0];
+    return arr && arr.length ? arr[0] : '';
+  },
+  isApplied() {
+    const user = Meteor.user();
+    if (!user) {
+      return false;
+    }
+
+    return user.profile.applied?.includes(this.issue.number);
   }
 });
